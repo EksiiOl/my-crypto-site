ad8534f4c0c85c06824e314cae21ae6a6aba2f4a
EksiiOl <2824.oleksii.0405@gmail.com>
2025-01-18 23:16:19 +0100

    Updated site

--- backend/server.js
+++ backend/server.js
@@ -49,6 +49,6 @@ app.post("/orders", (req, res) => {
 
 // Запуск сервера
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, "0.0.0.0", () => {
+app.listen(PORT, () => {
     console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
